4c29a9330372647fdb361611431a12f5be79e9bf
Derinsola16 <aderinsolaodusanya@gmail.com>
2021-08-03 08:55:20 +0100

    change

--- src/layout/Landing.vue
+++ src/layout/Landing.vue
@@ -1,15 +1,9 @@
 <template>
   <main class="d-flex">
-    <!-- <Profile /> -->
     <div class="flex-grow-1">
       <Nav />
       <div class="px-4 mb-5">
-        <!-- <div class="me-2 bottom-left w-25 ms-5" id="profile"> -->
-          <!-- <Profile /> -->
-        <!-- </div> -->
-        <!-- <div class="wide" id="image"> -->
-          <router-view />
-        <!-- </div> -->
+        <router-view />
       </div>
     </div>
   </main>
@@ -17,13 +11,11 @@
 
 <script>
 import Nav from "@/components/Nav.vue";
-// import Profile from "@/components/Profile.vue";
 
 export default {
   name: "Layout",
   components: {
     Nav,
-    // Profile,
   },
   data() {
     return {};
